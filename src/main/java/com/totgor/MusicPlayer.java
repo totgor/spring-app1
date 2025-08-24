f03c7c5fcf094005ec6aa4d93705d92a0b57a68d
totgor <totgor@mail.ru>
2025-08-24 13:35:59 +0300

    Внедрение зависимости - бина класса RockMusic, через анотацию @Autowired для поля класса PlayMusic в бин этого же класса PlayMusic.

--- src/main/java/com/totgor/MusicPlayer.java
+++ src/main/java/com/totgor/MusicPlayer.java
@@ -6,17 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
     
+    @Autowired
     private Music music;
     
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    @Autowired
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
     }
